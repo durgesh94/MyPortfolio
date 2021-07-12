4817c63b39c684d0c4d251050df495c59a63e2f1
@durgesh.tambe <durgesh.tambe94@gmail.com>
2021-07-12 23:51:29 +0530

    sidebar onClick fixes

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -8,22 +8,22 @@ export default function Mobile({ isOpen, setIsOpen }) {
                 <i className="fi-rr-cross"></i>
             </div>
             <div className="mobile-options">
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href="#projects">
                         <i className="fi-rr-edit-alt option-icon"></i>Projects
                     </a>
                 </div>
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href="#skills">
                         <i className="fi-rr-headset option-icon"></i>Skills
                     </a>
                 </div>
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href="#work">
                         <i className="fi-rr-briefcase option-icon"></i>Work
                     </a>
                 </div>
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href="#contact">
                     <i className="fi-rr-user option-icon"></i>Contact
                     </a>
