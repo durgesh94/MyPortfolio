593d0fa6ee5357671ee4b5f6073ebb40dc9b4479
durgesh94 <durgesh.tambe94@gmail.com>
2022-04-15 18:08:29 +0530

    update skills & projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -3,18 +3,27 @@ export const ProjectsData = [
         id: 1,
         title: "Covid-19 Tracker",
         about: "Covid 19 cases tracker by country wise shown on graph & count of the infected, recovered & death cases.",
-        tags: ["ReactJS"],
+        tags: ["ReactJS", "GitHub.io", "GitHub", "Git"],
         demo: "https://durgesh94.github.io/production",
         github: "https://github.com/durgesh94/react_covid-19",
         images: require("../../assets/images/covid19.png").default
     },
     {
-        id: 1,
-        title: "Covid-19 Tracker-2",
-        about: "Covid 19 cases tracker by country wise shown on graph & count of the infected, recovered & death cases.",
-        tags: ["ReactJS", "NodeJS", "MongoDB", "Heruko", "GitHub", "Git", "CI/CD", "ReactJS", "NodeJS", "MongoDB", "Heruko", "GitHub", "Git", "CI/CD"],
-        demo: "https://durgesh94.github.io/production",
-        github: "https://github.com/durgesh94/react_covid-19",
-        images: require("../../assets/images/covid19.png").default
+        id: 2,
+        title: "Kanban Board",
+        about: "Kanban is a project management method that helps visualize tasks, while Scrum is a method that provides structure to the team and schedule. Kanban and Scrum are project management methodologies that complete project tasks in small increments and emphasize continuous improvement. I has tried to build small kanban board using reactjs.",
+        tags: ["ReactJS", "Render", "GitHub", "Git"],
+        demo: "https://kanbanboard.onrender.com/",
+        github: "https://github.com/durgesh94/kanban-board",
+        images: require("../../assets/images/kanbanboard.png").default
+    },
+    {
+        id: 3,
+        title: "Booking Management System",
+        about: "A reservation management system is a digital application that securely collects and stores your property's bookings. There is mulitple type of booking is possible through this app, just for sample app have created.",
+        tags: ["React Native", "Expo", "GitHub", "Git"],
+        demo: "https://kanbanboard.onrender.com/",
+        github: "https://github.com/durgesh94/kanban-board",
+        images: require("../../assets/images/bms.png").default
     }
 ]
