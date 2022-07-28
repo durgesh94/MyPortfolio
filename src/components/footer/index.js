b140b05abc3a6bfce455706e8fcc7810ab24c273
durgesh94 <durgesh.tambe94@gmail.com>
2022-07-28 11:01:53 +0530

    update image, learning skills etc

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,6 +3,6 @@ import "./footer.css";
 
 export default function Footer() {
     return (
-        <div className="footer">Made with ReactJS by Durgesh @2022-April v2.0.0</div>
+        <div className="footer">Made with ReactJS & 8base by Durgesh @2022-July v2.0.1</div>
     )
 }
