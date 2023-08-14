e8a0b4ed307e0cfcc94d252ee6014192e4b021e6
Durgesh Tambe <dntambe@gmail.com>
2023-08-14 14:02:40 +0530

    update code after version upgrade

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,6 +3,6 @@ import "./footer.css";
 
 export default function Footer() {
     return (
-        <div className="footer">Made with ReactJS by Durgesh @2022-April v1.0.2</div>
+        <div className="footer">Made with ReactJS by Durgesh @2023-August v1.0.4</div>
     )
 }
